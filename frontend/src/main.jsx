2fe4cba7c4cbb4690935f1a4c564d0e5366bf069
Tio Jobs <icarojobsoficial@gmail.com>
2024-01-02 10:50:37 -0300

    Added small space to test project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import {QueryClientProvider} from "@tanstack/react-query";
 import queryClient from "./services/queryClient.js";
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
